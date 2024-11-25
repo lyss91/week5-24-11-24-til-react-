ccd08bf84093c38bc61890061a94348358d0fc35
lyss91 <leeyss1991@gmail.com>
2024-11-25 18:08:36 +0900

    25일 오후 수업 마무리

---
+++ src/components/pop-style.js
@@ -0,0 +1,3 @@
+// CSS 에 역할을 하는 객체 리터럴은 변수명을 파스칼로 합니다.(관례)
+export const TitleStyle = { color: "red", fontSize: "12px" };
+export const BodyStyle = { color: "greed", fontSize: "11px" };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import IndexPage from "./pages/indexPage";
+import Pop from "./components/Pop";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <IndexPage></IndexPage>
+    <Pop></Pop>
   </StrictMode>,
 );
